5ee8736d8e43faf28b04caa736a1d2b5ea538e1d
CuukyOfficial <just.cookie.jc@gmail.com>
2023-12-26 23:43:49 +0100

    Made Trekko implement

--- lib/controller/linked_trekko.dart
+++ lib/controller/linked_trekko.dart
@@ -1,7 +1,7 @@
 import 'package:app_backend/controller/trekko.dart';
 import 'package:app_backend/model/account/account_data.dart';
 
-class LinkedTrekko extends Trekko {
+class LinkedTrekko implements Trekko {
 
   final AccountData accountData;
 
--- lib/controller/trekko.dart
+++ lib/controller/trekko.dart
@@ -1,3 +1,5 @@
 abstract class Trekko {
 
+
+
 }
