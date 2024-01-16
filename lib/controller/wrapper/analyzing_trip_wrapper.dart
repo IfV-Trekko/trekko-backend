7e13b7e49208fb0d6ad7d24674b1cc8d059a320d
CuukyOfficial <just.cookie.jc@gmail.com>
2024-01-16 20:24:48 +0100

    Add Fluster

--- lib/controller/wrapper/analyzing_trip_wrapper.dart
+++ lib/controller/wrapper/analyzing_trip_wrapper.dart
@@ -1,10 +1,11 @@
-import 'package:app_backend/controller/utils/position_utils.dart';
+import 'package:app_backend/controller/wrapper/cluster_position.dart';
 import 'package:app_backend/controller/wrapper/leg/analyzing_leg_wrapper.dart';
 import 'package:app_backend/controller/wrapper/leg/leg_wrapper.dart';
 import 'package:app_backend/controller/wrapper/trip_wrapper.dart';
 import 'package:app_backend/model/trip/donation_state.dart';
 import 'package:app_backend/model/trip/leg.dart';
 import 'package:app_backend/model/trip/trip.dart';
+import 'package:fluster/fluster.dart';
 import 'package:geolocator_platform_interface/src/models/position.dart';
 
 class AnalyzingTripWrapper implements TripWrapper {
@@ -13,13 +14,49 @@ class AnalyzingTripWrapper implements TripWrapper {
 
   @override
   Future<double> calculateEndProbability() {
-    return PositionUtils.calculateEndProbability(
-        Duration(minutes: 15),
-        9,
-        _legs
-            .expand(
-                (element) => element.trackedPoints.map((t) => t.toPosition()))
-            .toList());
+    return Future.microtask(() {
+      final fluster = Fluster<ClusterPosition>(
+          minZoom: 0,
+          maxZoom: 17,
+          radius: 150,
+          extent: 512,
+          nodeSize: 64,
+          points: _legs
+              .expand((element) => element.trackedPoints
+              .map((e) => ClusterPosition(
+              latitude: e.latitude, longitude: e.longitude))
+              .toList())
+              .toList(),
+          createCluster:
+              (BaseCluster cluster, double longitude, double latitude) {
+            return ClusterPosition(
+                latitude: latitude, longitude: longitude, clusterId: cluster.id);
+          });
+
+      List<double> bounds = [];
+
+      _legs.forEach((leg) {
+        leg.trackedPoints.forEach((point) {
+          if (bounds.length == 0) {
+            bounds.add(point.longitude);
+            bounds.add(point.latitude);
+            bounds.add(point.longitude);
+            bounds.add(point.latitude);
+          } else {
+            if (point.longitude < bounds[0]) bounds[0] = point.longitude;
+            if (point.latitude < bounds[1]) bounds[1] = point.latitude;
+            if (point.longitude > bounds[2]) bounds[2] = point.longitude;
+            if (point.latitude > bounds[3]) bounds[3] = point.latitude;
+          }
+        });
+      });
+
+      double probability = 0;
+      List<ClusterPosition> clusters = fluster.clusters(bounds, 17);
+
+
+      return probability;
+    });
   }
 
   @override
---
+++ lib/controller/wrapper/cluster_position.dart
@@ -0,0 +1,6 @@
+import 'package:fluster/fluster.dart';
+
+class ClusterPosition extends Clusterable {
+  ClusterPosition({latitude, longitude, clusterId})
+      : super(latitude: latitude, longitude: longitude, clusterId: clusterId);
+}
