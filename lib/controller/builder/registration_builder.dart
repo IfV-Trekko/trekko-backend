172eb6b5b7b964186d612b63e81d83a4d6fc820e
CuukyOfficial <just.cookie.jc@gmail.com>
2024-01-23 22:11:47 +0100

    remove: sending email code

--- lib/controller/builder/registration_builder.dart
+++ lib/controller/builder/registration_builder.dart
@@ -2,7 +2,6 @@ import 'package:app_backend/controller/builder/build_exception.dart';
 import 'package:app_backend/controller/builder/registration_result.dart';
 import 'package:app_backend/controller/builder/trekko_builder.dart';
 import 'package:app_backend/controller/request/bodies/request/auth_request.dart';
-import 'package:app_backend/controller/request/bodies/request/code_request.dart';
 import 'package:app_backend/controller/request/bodies/response/auth_response.dart';
 import 'package:app_backend/controller/request/trekko_server.dart';
 import 'package:app_backend/controller/request/url_trekko_server.dart';
@@ -40,7 +39,7 @@ class RegistrationBuilder extends TrekkoBuilder {
         .signUp(AuthRequest(_email, _password))
         .catchError(onError<AuthResponse>)
         .then((value) async {
-      await _server.confirmEmail(CodeRequest(_code));
+      // await _server.confirmEmail(CodeRequest(_code)); // TODO: exc handling
       return makeTrekko(_projectUrl, _email, value.token);
     });
   }
