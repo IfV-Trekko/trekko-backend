ee1b0e7f0709d7aad240bd6a5cb652f61a3719d5
CuukyOfficial <just.cookie.jc@gmail.com>
2024-01-25 18:03:32 +0100

    remove: unused classes

--- lib/controller/analysis/analysis_builder.dart
+++
@@ -1,8 +0,0 @@
-import 'package:app_backend/controller/analysis/calculation_reductor.dart';
-import 'package:app_backend/model/trip/trip.dart';
-import 'package:isar/isar.dart';
-
-abstract class AnalysisBuilder {
-  Stream<T?> build<T>(
-      Query<Trip> trips, T Function(Trip) tripData, Reduction<T> reduction);
-}
