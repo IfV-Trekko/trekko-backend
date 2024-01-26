8efd5aaf7003fb9edd891a640d9f88f9e2a2e392
CuukyOfficial <just.cookie.jc@gmail.com>
2024-01-26 17:58:10 +0100

    feat: tests for trekko working

---
+++ test/profile_test.dart
@@ -0,0 +1,36 @@
+import 'package:app_backend/controller/trekko.dart';
+import 'package:app_backend/model/profile/profile.dart';
+import 'package:test/test.dart';
+
+import 'trekko_build_utils.dart';
+
+const String password = "1aA!hklj32r4hkjl324r";
+const String email = "profile_test112@profile_test.com";
+
+void main() {
+  late Trekko trekko;
+  setUpAll(() async => trekko = await TrekkoBuildUtils().loginOrRegister(email, password));
+
+  test("Profile data correct", () async {
+    Profile profile = await trekko.getProfile().first;
+    expect(profile.email, equals(email));
+  });
+
+  test("Set and get question answer", () async {
+    Profile profile = await trekko.getProfile().first;
+    profile.preferences.setQuestionAnswer("homeOffice", true);
+    profile.preferences.setQuestionAnswer("gender", "female");
+    profile.preferences.setQuestionAnswer("age", 21);
+
+    expect(profile.preferences.getQuestionAnswer("homeOffice"), equals(true));
+    expect(profile.preferences.getQuestionAnswer("gender"), equals("female"));
+    expect(profile.preferences.getQuestionAnswer("age"), equals(21));
+
+
+    await trekko.savePreferences(profile.preferences);
+    profile = await trekko.getProfile().first;
+    expect(profile.preferences.getQuestionAnswer("homeOffice"), equals(true));
+    expect(profile.preferences.getQuestionAnswer("gender"), equals("female"));
+    expect(profile.preferences.getQuestionAnswer("age"), equals(21));
+  });
+}
---
+++ test/trekko_build_utils.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:app_backend/controller/builder/build_exception.dart';
+import 'package:app_backend/controller/builder/login_builder.dart';
+import 'package:app_backend/controller/builder/login_result.dart';
+import 'package:app_backend/controller/builder/registration_builder.dart';
+import 'package:app_backend/controller/trekko.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:isar/isar.dart';
+import 'package:mockito/mockito.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+class MockPathProvider extends Mock
+    with MockPlatformInterfaceMixin implements PathProviderPlatform {
+
+  @override
+  Future<String?> getApplicationDocumentsPath() async {
+    return (Directory.systemTemp).path;
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {}
+
+class TrekkoBuildUtils {
+  Future<Trekko> loginOrRegister(String email, String password) async {
+    // DartPluginRegistrant.ensureInitialized();
+    TestWidgetsFlutterBinding.ensureInitialized();
+    HttpOverrides.global = MyHttpOverrides();
+    await Isar.initializeIsarCore(download: true);
+    PathProviderPlatform.instance = MockPathProvider();
+    disablePathProviderPlatformOverride = true;
+    late String ip;
+    if (Platform.isAndroid) {
+      ip = "10.0.2.2";
+    } else {
+      ip = "localhost";
+    }
+    try {
+      return await LoginBuilder("http://$ip:8080", email, password).build();
+    } catch (e) {
+      if (e is BuildException) {
+        if (e.reason == LoginResult.failedNoSuchUser) {
+          try {
+            return await RegistrationBuilder(
+                    "http://$ip:8080", email, password, password, "12345")
+                .build();
+          } catch (e) {
+            print((e as BuildException).reason);
+          }
+        }
+      }
+      rethrow;
+    }
+  }
+}
