57d8e957f8646a7145f55e4a10b3907b9b865bab
CuukyOfficial <just.cookie.jc@gmail.com>
2024-01-27 22:31:43 +0100

    fix: use schemas provided

--- lib/controller/utils/database_utils.dart
+++ lib/controller/utils/database_utils.dart
@@ -1,14 +1,12 @@
-import 'package:app_backend/model/profile/profile.dart';
-import 'package:app_backend/model/trip/trip.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DatabaseUtils {
-
-  static Future<Isar> establishConnection(List<CollectionSchema<dynamic>> schemas) async {
+  static Future<Isar> establishConnection(
+      List<CollectionSchema<dynamic>> schemas) async {
     var dir = await getApplicationDocumentsDirectory();
     return await Isar.open(
-      [TripSchema, ProfileSchema],
+      schemas,
       directory: dir.path,
     );
   }
