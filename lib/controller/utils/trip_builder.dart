46b5e6265466c7da264f4a4bddea530f28263029
CuukyOfficial <just.cookie.jc@gmail.com>
2024-01-29 18:26:57 +0100

    fix: trip builder leg

--- lib/controller/utils/trip_builder.dart
+++ lib/controller/utils/trip_builder.dart
@@ -11,6 +11,7 @@ double latDegreesPerMeter = 1 / metersPerLatDegree;
 class TripBuilder {
 
   final List<Leg> _legs = List.empty(growable: true);
+  List<TrackedPoint> _leg = List.empty(growable: true);
   DateTime time = DateTime.now();
   double latitude = 49.006889;
   double longitude = 8.403653;
@@ -21,12 +22,10 @@ class TripBuilder {
 
   TripBuilder stay(Duration duration) {
     DateTime end = time.add(duration);
-    List<TrackedPoint> stay = [];
     while (time.isBefore(end)) {
-      stay.add(TrackedPoint.withData(latitude, longitude, 0, time));
+      _leg.add(TrackedPoint.withData(latitude, longitude, 0, time));
       time = time.add(Duration(seconds: 5));
     }
-    _legs.add(Leg.withData(TransportType.by_foot, stay));
     return this;
   }
 
@@ -36,25 +35,32 @@ class TripBuilder {
 
   TripBuilder move(
       bool forward, Duration duration, Distance distance) {
-    List<TrackedPoint> leg = [];
     var speed = distance.per(duration.inSeconds.seconds);
     var speedAsMetersPerSecond = speed.as(meters, seconds);
     double forwardMultiplier = forward ? 1 : -1;
     double latAddPer5Sec = speedAsMetersPerSecond * latDegreesPerMeter * 5 * forwardMultiplier;
     DateTime end = time.add(duration);
     while (time.isBefore(end)) {
-      leg.add(TrackedPoint.withData(
+      _leg.add(TrackedPoint.withData(
           latitude, longitude, speed.as(kilo.meters, hours), time));
       latitude += latAddPer5Sec;
       time = time.add(Duration(seconds: 5));
     }
-    leg.add(TrackedPoint.withData(
+    _leg.add(TrackedPoint.withData(
         latitude, longitude, speed.as(kilo.meters, hours), time));
-    _legs.add(Leg.withData(TransportType.by_foot, leg));
+    return this;
+  }
+
+  TripBuilder leg() {
+    if (_leg.isNotEmpty) {
+      _legs.add(Leg.withData(TransportType.by_foot, _leg));
+    }
+    _leg = List.empty(growable: true);
     return this;
   }
 
   Trip build() {
+    this.leg();
     return Trip.withData(_legs);
   }
 }
