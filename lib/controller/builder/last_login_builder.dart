d8968fe3f11a99efadc0ab5e105f3fecda622d53
CuukyOfficial <just.cookie.jc@gmail.com>
2024-02-01 17:38:22 +0100

    fix: last login builder

--- lib/controller/builder/last_login_builder.dart
+++ lib/controller/builder/last_login_builder.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:app_backend/controller/builder/login_result.dart';
 import 'package:app_backend/controller/builder/trekko_builder.dart';
-import 'package:app_backend/controller/profiled_trekko.dart';
 import 'package:app_backend/controller/trekko.dart';
 import 'package:app_backend/controller/utils/database_utils.dart';
 import 'package:app_backend/model/profile/profile.dart';
@@ -33,10 +32,7 @@ class LastLoginBuilder extends TrekkoBuilder {
 
       Profile? latestProfile =
           await value.profiles.where().sortByLastLoginDesc().findFirst();
-      return ProfiledTrekko(
-          projectUrl: latestProfile!.projectUrl,
-          email: latestProfile.email,
-          token: latestProfile.token);
+      return makeTrekko(latestProfile!.projectUrl, latestProfile.email, latestProfile.token);
     });
   }
 }
