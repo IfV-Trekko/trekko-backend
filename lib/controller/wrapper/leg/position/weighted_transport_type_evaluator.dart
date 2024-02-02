1607304aa14a44dafd15f0202f0b3c9cb5a8cdc4
CuukyOfficial <just.cookie.jc@gmail.com>
2024-02-02 10:16:32 +0100

    fix: transport type calculation

--- lib/controller/wrapper/leg/position/weighted_transport_type_evaluator.dart
+++ lib/controller/wrapper/leg/position/weighted_transport_type_evaluator.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:app_backend/controller/wrapper/leg/position/transport_type_data_provider.dart';
 import 'package:app_backend/controller/wrapper/leg/position/transport_type_evaluator.dart';
 import 'package:app_backend/model/trip/leg.dart';
@@ -13,9 +15,12 @@ class WeightedTransportTypeEvaluator implements TransportTypeEvaluator {
   Future<double> evaluate(Leg leg) {
     return Future.microtask(() async {
       double providedSpeed =
-          (await dataProvider.getAverageSpeed()).as(meters, seconds);
-      double legSpeed = leg.getSpeed().as(meters, seconds);
-      return legSpeed / providedSpeed;
+          (await dataProvider.getAverageSpeed()).as(kilo.meters, hours);
+      double legSpeed = leg.getSpeed().as(kilo.meters, hours);
+      double distanceBetweenSpeeds = (providedSpeed - legSpeed).abs();
+      // f(x) = -x^2 * 1 * 10^-6 + 1
+      double calculated = -pow(distanceBetweenSpeeds, 2) * pow(10, -6) + 1;
+      return max(calculated, 1);
     });
   }
 
