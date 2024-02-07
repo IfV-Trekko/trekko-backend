66a196ff13cb9506b8cda5cf7b002aa1f1cf32f0
CuukyOfficial <just.cookie.jc@gmail.com>
2024-02-07 13:59:24 +0100

    fix: location permission listener

--- lib/controller/profiled_trekko.dart
+++ lib/controller/profiled_trekko.dart
@@ -79,14 +79,14 @@ class ProfiledTrekko implements Trekko {
       _profileId = await _saveProfile(found);
     }
   }
-
-  Future<void> _listenForLocationPermission() async {
-    await Geolocator.getServiceStatusStream().listen((event) {
-      if (event == ServiceStatus.disabled) {
-        setTrackingState(TrackingState.paused);
-      }
-    });
-  }
+  //
+  // Future<void> _listenForLocationPermission() async {
+  //   await Geolocator.getServiceStatusStream().listen((event) {
+  //     if (event == ServiceStatus.disabled) {
+  //       setTrackingState(TrackingState.paused);
+  //     }
+  //   });
+  // }
 
   Future<void> _startTracking() async {
     if (!(await LocationBackgroundTracking.isRunning()))
@@ -146,7 +146,7 @@ class ProfiledTrekko implements Trekko {
     _profileDb = await DatabaseUtils.openProfiles();
     await _initProfile();
     _tripDb = await DatabaseUtils.openTrips(this._profileId);
-    await _listenForLocationPermission();
+    // await _listenForLocationPermission();
 
     if ((await getProfile().first).trackingState == TrackingState.running) {
       await _startTracking();
