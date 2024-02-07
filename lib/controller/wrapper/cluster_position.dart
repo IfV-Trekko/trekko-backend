2d25d64830703800774726f1a8781c6868596a19
CuukyOfficial <just.cookie.jc@gmail.com>
2024-02-07 16:01:08 +0100

    remove: legacy cluster_position.dart

--- lib/controller/wrapper/cluster_position.dart
+++
@@ -1,6 +0,0 @@
-import 'package:fluster/fluster.dart';
-
-class ClusterPosition extends Clusterable {
-  ClusterPosition({latitude, longitude, clusterId})
-      : super(latitude: latitude, longitude: longitude, clusterId: clusterId);
-}
