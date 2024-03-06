9915b7ec7dac14cf1a8f686c20d6134dd63a49a3
Matthias <33065597+matthiaaas@users.noreply.github.com>
2024-03-06 16:32:04 +0100

    feat: remove `deleteProfile` method from auth utils

--- lib/controller/builder/authentification_utils.dart
+++ lib/controller/builder/authentification_utils.dart
@@ -1,38 +1,10 @@
 import 'package:app_backend/controller/request/bodies/request/send_code_request.dart';
 import 'package:app_backend/controller/request/endpoint.dart';
-import 'package:app_backend/controller/request/trekko_server.dart';
 import 'package:app_backend/controller/request/url_trekko_server.dart';
-import 'package:app_backend/controller/utils/database_utils.dart';
-import 'package:app_backend/model/profile/profile.dart';
-import 'package:isar/isar.dart';
 
 class AuthentificationUtils {
   static Future<void> sendCode(String projectUrl, String email) {
     return UrlTrekkoServer(projectUrl).sendCode(SendCodeRequest(email));
-  }
-
-  static Future<bool> deleteProfile(String projectUrl, String email) async {
-    Isar db = await DatabaseUtils.openProfiles();
-    Profile? profile = await db.profiles
-        .filter()
-        .projectUrlEqualTo(projectUrl)
-        .and()
-        .emailEqualTo(email)
-        .findFirst();
-    if (profile == null) {
-      return false;
-    }
-
-    Isar tripDb = await DatabaseUtils.openTrips(profile.id);
-    TrekkoServer server = UrlTrekkoServer.withToken(projectUrl, profile.token);
-    return server.deleteAccount().then((value) async {
-      return db.writeTxn(() async => db.profiles.delete(profile.id));
-    }).then((value) {
-      return tripDb.close(deleteFromDisk: true);
-    }).then((value) async {
-      await db.close();
-      return value;
-    });
   }
 
   static Future<bool> isServerValid(String projectUrl) async {
