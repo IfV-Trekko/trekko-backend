23b7af838a08a1aa74b06a339171ce577fd93cce
CuukyOfficial <just.cookie.jc@gmail.com>
2024-05-08 17:07:06 +0200

    fix: location accuracy

--- lib/model/profile/battery_usage_setting.dart
+++ lib/model/profile/battery_usage_setting.dart
@@ -3,9 +3,9 @@ import 'package:geolocator/geolocator.dart';
 
 // Leave order and names as is!
 enum BatteryUsageSetting {
-  low(60, LocationAccuracy.low, 40),
-  medium(35, LocationAccuracy.medium, 15),
-  high(20, LocationAccuracy.high, 2);
+  low(60, LocationAccuracy.best, 40),
+  medium(35, LocationAccuracy.best, 15),
+  high(20, LocationAccuracy.best, 2);
 
   final int interval;
   final LocationAccuracy accuracy;
